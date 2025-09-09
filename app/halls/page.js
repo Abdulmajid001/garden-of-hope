e011a739a2c41aae51ccc588e6e7a76cdf022da9
Abdulmajid001 <157312702+Abdulmajid001@users.noreply.github.com>
2025-09-09 19:34:07 +0100

    Update page.js

--- app/halls/page.js
+++ app/halls/page.js
@@ -8,7 +8,7 @@ export const metadata ={
 };
 
 const Page = () => (
-  <div className='max-w-[130rem] mx-auto py-[9.6rem] px-[4.6rem]'>
+  <div className='max-w-[130rem] mx-auto py-[9.6rem] px-[2rem] md:px-[4.6rem]'>
     <HeadingMainGreen heading="Elegant Spaces for Every Memorable Occasion" description="Host unforgettable gatherings in our versatile event halls, where timeless design meets tranquil surroundings. From intimate celebrations to grand affairs, every detail is thoughtfully curated to reflect your vision and provide a seamless, inspiring experience for you and your guests." />
     <Suspense fallback={<Loader />}>
       <HallList />
