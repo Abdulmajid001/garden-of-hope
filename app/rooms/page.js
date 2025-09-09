b53f1f790a1b56acbedfdf70e22296f1da3070fc
Abdulmajid001 <157312702+Abdulmajid001@users.noreply.github.com>
2025-09-09 19:33:08 +0100

    Update page.js

--- app/rooms/page.js
+++ app/rooms/page.js
@@ -8,7 +8,7 @@ export const metadata ={
 };
 
 const Page = () => (
-  <div className='max-w-[130rem] mx-auto py-[9.6rem] px-[4.6rem]'>
+  <div className='max-w-[130rem] mx-auto py-[9.6rem] px-[2rem] md:px-[4.6rem]'>
     <HeadingMainGreen heading="Discover Comfort & Serenity in Every Room" description="Unwind in thoughtfully designed spaces that blend natural beauty, modern elegance, and warm hospitality. Each of our rooms offers a unique sanctuary where comfort meets charm—complete with lush garden views, soothing décor, and all the amenities you need for a truly rejuvenating stay at Garden of Hope Hotel." />
     <Suspense fallback={<Loader />}>
       <RoomList />
