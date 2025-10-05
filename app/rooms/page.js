dbc863a5a0e91431643a8ec727cbda983e94c042
Abdulmajid001 <157312702+Abdulmajid001@users.noreply.github.com>
2025-10-05 14:46:51 +0100

    Update page.js

--- app/rooms/page.js
+++ app/rooms/page.js
@@ -3,7 +3,6 @@ import {HeadingMainGreen} from '../_components/HeadingMain';
 import Loader from '../_components/Loader';
 import RoomList from '../_components/RoomList';
 
-export const revalidate = 60;
 
 export const metadata ={
   title:'Rooms'
