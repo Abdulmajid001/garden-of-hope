9010b50dbe564039ec66362c48869aaa334494a7
Abdulmajid <abdulmajidhope@gmail.com>
2025-10-18 07:54:54 +0100

    update

--- app/_components/PageHeading.js
+++ app/_components/PageHeading.js
@@ -3,7 +3,7 @@ import React from 'react'
 function PageHeading({ heading, description }) {
   return (
     <div className="flex flex-col gap-8 mb-[4rem]">
-      <h2 className="text-green-dark  font-bold text-5xl md:text-6xl">{heading}</h2>
+      <h2 className="text-green-dark  font-bold text-4xl md:text-6xl">{heading}</h2>
       <p className="text-green-dark text-2xl md:text-3xl">{description}</p>
     </div>
   )
